51a35a89dec7325756d08834120cf7ea1fed62b7
StephMGibbs <StillSeven5011@gmail.com>
2020-02-15 16:44:35 -0800

    Initial commit

---
+++ src/loops.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class loops {
+    public static void main (String[] args){
+        System.out.println("Howdy y'all I'm: ");
+        Scanner scnr = new Scanner(System.in);
+        String name = scnr.nextLine();
+        System.out.println("Hello " + name + "!");
+    }
+}
