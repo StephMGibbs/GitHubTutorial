60722917b1081c8bb3e4e7a9265d22a0222d6186
StephMGibbs <60988702+StephMGibbs@users.noreply.github.com>
2020-02-15 17:04:54 -0800

    Update loops.java
    added code for loops.java

--- src/loops.java
+++ src/loops.java
@@ -1,9 +1,34 @@
-import java.util.*;
+package org.launchcode.java.demos;
+
+/**
+ * Created by LaunchCode
+ */
 public class loops {
-    public static void main (String[] args){
-        System.out.println("Howdy y'all I'm: ");
-        Scanner scnr = new Scanner(System.in);
-        String name = scnr.nextLine();
-        System.out.println("Hello " + name + "!");
+
+    public static void main(String[] args) {
+
+        System.out.println("printZeroToTen :: ");
+        printZeroToTen();
+
+        System.out.println("printCharactersFromAString :: ");
+        printCharactersFromAString();
+
+    }
+
+    // Print integers from 0 to 10
+    public static void printZeroToTen() {
+
+        for (int i = 0; i < 11; i++) {
+            System.out.println(i);
+        }
+    }
+
+    public static void printCharactersFromAString() {
+        String text = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book...";
+
+        for (char c : text.toCharArray()) {
+            System.out.print(c);
+        }
     }
 }
+
